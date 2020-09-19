588b4e7c8c30359bb3cc93042037306580d8111f
asteriation <asteriation@isomorphicbox.com>
2020-09-19 03:25:51 -0700

    added array bfevfl code generation

---
+++ bfevfl/array.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from typing import Optional, Sequence
+
+from bitstring import BitStream, pack
+
+from .str_ import String
+from .block import Block, DataBlock, ContainerBlock
+
+class BlockPtrArray(DataBlock):
+    def __init__(self, items: Sequence[Block]) -> None:
+        super().__init__(8 * len(items))
+
+        for i, item in enumerate(items):
+            self._add_pointer(8 * i, item)
+
+    def alignment(self) -> int:
+        return 8
+
+class IntArray(DataBlock):
+    def __init__(self, items: Sequence[int]) -> None:
+        super().__init__(4 * len(items))
+
+        for i, item in enumerate(items):
+            self.buffer.overwrite(pack('uintle:32', item))
+
+    def alignment(self) -> int:
+        return 4
+
+class BoolArray(DataBlock):
+    # todo: verify true
+    def __init__(self, items: Sequence[bool]) -> None:
+        super().__init__(4 * len(items))
+
+        for i, item in enumerate(items):
+            self.buffer.overwrite(pack('uintle:32', 0x80000001 if item else 0x00000000))
+
+    def alignment(self) -> int:
+        return 4
+
+class FloatArray(DataBlock):
+    def __init__(self, items: Sequence[float]) -> None:
+        super().__init__(4 * len(items))
+
+        for i, item in enumerate(items):
+            self.buffer.overwrite(pack('floatle:32', item))
+
+    def alignment(self) -> int:
+        return 4
+
+class StringArray(ContainerBlock):
+    def __init__(self, items: Sequence[str]) -> None:
+        sobj = [String(item) for item in items]
+        for s in sobj:
+            s.alignment = lambda: 8
+
+        super().__init__(sobj)
+
+    def alignment(self) -> int:
+        return 8
+
---
+++ bfevfl/test_array.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import unittest
+
+from bitstring import Bits, pack
+
+from .block import Block
+from .array import BlockPtrArray, IntArray, BoolArray, FloatArray, StringArray
+
+Block.__abstractmethods__ = frozenset()
+
+class TestArray(unittest.TestCase):
+    def test_blockptr_empty(self):
+        arr = BlockPtrArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(''))
+
+    def test_blockptr(self):
+        b = Block()
+        b.set_offset(0xbaadf00d)
+
+        arr = BlockPtrArray([b, None])
+        self.assertEqual(arr.prepare_bitstream(),
+                pack('uintle:64', b.offset) + pack('uintle:64', 0)
+        )
+
+    def test_int_empty(self):
+        arr = IntArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(''))
+
+    def test_int(self):
+        arr = IntArray([0x12345678, 0x9abcdef0])
+        self.assertEqual(arr.prepare_bitstream(),
+                Bits(b'\x78\x56\x34\x12\xf0\xde\xbc\x9a')
+        )
+
+    def test_bool_empty(self):
+        arr = BoolArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(''))
+
+    def test_bool(self):
+        arr = BoolArray([False, True])
+        self.assertEqual(arr.prepare_bitstream(),
+                Bits(b'\0\0\0\0\1\0\0\x80')
+        )
+
+    def test_float_empty(self):
+        arr = FloatArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(''))
+
+    def test_float(self):
+        arr = FloatArray([500.0, -200.1])
+        self.assertEqual(arr.prepare_bitstream(),
+                pack('floatle:32', 500.0) + pack('floatle:32', -200.1)
+        )
+
+    def test_string_empty(self):
+        arr = StringArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(''))
+
+    def test_string(self):
+        arr = StringArray(['ab', 'zxcvzxcvzxcv', 'vv'])
+        self.assertEqual(arr.prepare_bitstream(),
+                Bits(b'\2\0ab\0\0\0\0') +
+                Bits(b'\x0c\0zxcvzxcvzxcv\0\0') +
+                Bits(b'\2\0vv\0')
+        )
+
