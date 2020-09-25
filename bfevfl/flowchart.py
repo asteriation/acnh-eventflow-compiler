c2bb7b32f43c7bdc066407a5e033e71ac5b0ab9d
asteriation <asteriation@isomorphicbox.com>
2020-09-25 02:21:54 -0700

    added bfevfl flowchart generation

---
+++ bfevfl/flowchart.py
@@ -0,0 +1,300 @@
+from __future__ import annotations
+
+from collections import OrderedDict
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+from bitstring import BitStream, pack
+
+from .datatype import TypedValue
+from .actors import Actor
+from .nodes import Node, RootNode, ActionNode
+from .block import DataBlock, ContainerBlock, Block
+from .str_ import StringPool, String
+from .dic_ import Dictionary
+from .array import BlockPtrArray, BlockArray
+from .container import Container
+
+class _Actor(DataBlock):
+    def __init__(self, name: str, sec_name: str, actions: Optional[BlockPtrArray[String]],
+                 queries: Optional[BlockPtrArray[String]], pool: StringPool) -> None:
+        super().__init__(55)
+        self._add_pointer(0, pool[name])
+        self._add_pointer(8, pool[sec_name])
+        self._add_pointer(16, pool.empty)
+        self._add_pointer(24, actions)
+        self._add_pointer(32, queries)
+        self._add_pointer(40, None)
+
+        with self._at_offset(48):
+            self.buffer.overwrite(pack('uintle:16', actions.n if actions is not None else 0))
+            self.buffer.overwrite(pack('uintle:16', queries.n if queries is not None else 0))
+            self.buffer.overwrite(pack('uintle:16', 0xFFFF))
+            self.buffer.overwrite(pack('uintle:8', 1))
+
+    def alignment(self) -> int:
+        return 8
+
+_EventData = Union[Container]
+
+class _Event(DataBlock):
+    def  __init__(self, name: str, event_type: int, pool: StringPool) -> None:
+        super().__init__(0x28)
+
+        self._add_pointer(0, pool[name])
+        with self._at_offset(8):
+            self.buffer.overwrite(pack('uintle:8', event_type))
+
+    def alignment(self) -> int:
+        return 8
+
+class _ActionEvent(_Event):
+    def __init__(self, name: str, next_index: int, actor_index: int, action_index: int,
+                 params: Optional[Container], pool: StringPool) -> None:
+        super().__init__(name, 0, pool)
+
+        with self._at_offset(0xa):
+            self.buffer.overwrite(pack('uintle:16', next_index))
+            self.buffer.overwrite(pack('uintle:16', actor_index))
+            self.buffer.overwrite(pack('uintle:16', action_index))
+
+        self._add_pointer(0x10, params)
+
+# TODO query
+
+class _SubflowIndexArray(DataBlock):
+    def __init__(self, subflow_indices: List[int]) -> None:
+        super().__init__(len(subflow_indices) * 2)
+
+        self.n = len(subflow_indices)
+        with self._at_offset(0):
+            for i in subflow_indices:
+                self.buffer.overwrite(pack('uintle:16', i))
+
+    def alignment(self) -> int:
+        return 8
+
+class _VarDefData(DataBlock):
+    def alignment(self) -> int:
+        return 8
+
+class _IntVarDefData(_VarDefData):
+    def __init__(self, value: int) -> None:
+        super().__init__(4)
+        with self._at_offset(0):
+            self.buffer.overwrite(pack('uintle:32', value))
+
+class _BoolVarDefData(_VarDefData):
+    def __init__(self, value: bool) -> None:
+        super().__init__(4)
+        with self._at_offset(0):
+            self.buffer.overwrite(pack('uintle:32', 0x80000001 if value else 0))
+
+class _FloatVarDefData(_VarDefData):
+    def __init__(self, value: float) -> None:
+        super().__init__(4)
+        with self._at_offset(0):
+            self.buffer.overwrite(pack('floatle:32', value))
+
+class _VarDefFooter(DataBlock):
+    def __init__(self, type_: int, count: int) -> None:
+        # other types should be supported, but unsure how they
+        # are handled -- it's probably a pointer, but where the pointed data
+        # resides is unknown - ACNH has no such usage
+        assert type_ in (2, 3, 4) # int, bool, float
+
+        super().__init__(4)
+        with self._at_offset(0):
+            self.buffer.overwrite(pack('uintle:16', count))
+            self.buffer.overwrite(pack('uintle:16', type_))
+
+    def alignment(self) -> int:
+        return 8
+
+class _VarDef(ContainerBlock):
+    def __init__(self, value: TypedValue) -> None:
+        # data (64 bit align) + footer (64 bit align)
+        # unsure how most types are handled
+        parts: List[Block] = []
+        if value.type.type.startswith('int') or value.type.type.startswith('enum'):
+            assert isinstance(value.value, int)
+            parts = [
+                _IntVarDefData(value.value),
+                _VarDefFooter(2, 1),
+            ]
+        elif value.type.type == 'bool':
+            assert isinstance(value.value, bool)
+            parts = [
+                _BoolVarDefData(value.value),
+                _VarDefFooter(3, 1),
+            ]
+        elif value.type.type == 'float':
+            assert isinstance(value.value, float)
+            parts = [
+                _FloatVarDefData(value.value),
+                _VarDefFooter(4, 1),
+            ]
+        else:
+            raise ValueError(f'unsupported vardef type: {value.type}')
+
+        super().__init__(parts)
+
+    def alignment(self) -> int:
+        return 8
+
+class _Pad24(DataBlock):
+    def __init__(self) -> None:
+        super().__init__(24)
+
+    def alignment(self) -> int:
+        return 8
+
+# there is a 24-byte padding per entrypoint after SubflowIndexArray, unless
+# the entrypoint has vardefs
+_EntrypointData = Union[BlockArray[_VarDef], _Pad24]
+
+class _Entrypoint(DataBlock):
+    def __init__(self, vardef_names: Optional[Dictionary], vardefs: _EntrypointData,
+                 subflow_indices: Optional[_SubflowIndexArray], main_index: int) -> None:
+        super().__init__(0x1e)
+
+        self._add_pointer(0, subflow_indices)
+        self._add_pointer(8, vardef_names)
+        self._add_pointer(16, vardefs if isinstance(vardefs, BlockArray) else None)
+
+        with self._at_offset(24):
+            self.buffer.overwrite(pack('uintle:16', subflow_indices.n if subflow_indices else 0))
+            self.buffer.overwrite(pack('uintle:16', vardefs.n if isinstance(vardefs, BlockArray) else 0))
+            self.buffer.overwrite(pack('uintle:16', main_index))
+
+    def alignment(self) -> int:
+        return 8
+
+class _FlowchartHeader(DataBlock):
+    def __init__(self, name: str, num_actions: int, num_queries: int, actors: BlockArray[_Actor],
+            events: BlockArray[_Event], entrypoint_names: Dictionary,
+            entrypoints: BlockArray[_Entrypoint], pool: StringPool) -> None:
+        super().__init__(0x48)
+
+        self.pool = pool
+        with self._at_offset(0):
+            self.buffer.overwrite(b'EVFL')
+        with self._at_offset(0x10):
+            self.buffer.overwrite(pack('uintle:16', actors.n))
+            self.buffer.overwrite(pack('uintle:16', num_actions))
+            self.buffer.overwrite(pack('uintle:16', num_queries))
+            self.buffer.overwrite(pack('uintle:16', events.n))
+            self.buffer.overwrite(pack('uintle:16', entrypoints.n))
+        self._add_pointer(0x20, pool[name].c_str)
+        self._add_pointer(0x28, actors)
+        self._add_pointer(0x30, events)
+        self._add_pointer(0x38, entrypoint_names)
+        self._add_pointer(0x40, entrypoints)
+
+    def prepare_bitstream(self) -> BitStream:
+        with self._at_offset(4):
+            self.buffer.overwrite(pack('uintle:32', self.pool.offset - self.offset))
+        return super().prepare_bitstream()
+
+    def alignment(self) -> int:
+        return 8
+
+class Flowchart(ContainerBlock):
+    def __init__(self, name: str, actors_: List[Actor], events_: List[Node],
+                 entrypoints_: List[RootNode], pool: StringPool) -> None:
+        actors: List[_Actor] = []
+        events: List[_Event] = []
+        entrypoint_names: List[str] = [] # -> Dictionary
+        entrypoints: List[_Entrypoint] = []
+        event_data: List[_EventData] = []
+        actor_fnames: List[BlockPtrArray[String]] = []
+        entrypoint_data: List[_EntrypointData] = []
+
+        num_actions = 0
+        num_queries = 0
+
+        actor_indices: Dict[str, int] = {}
+        action_indices: Dict[str, Dict[str, int]] = {}
+        query_indices: Dict[str, Dict[str, int]] = {}
+        for actor in actors_:
+            actions = [n for n, a in actor.actions.items() if a.used]
+            queries = [n for n, q in actor.queries.items() if q.used]
+            num_actions += len(actions)
+            num_queries += len(queries)
+
+            if not actions and not queries:
+                continue
+
+            actor_indices[actor.name] = len(actor_indices)
+            action_indices[actor.name] = {n: i for i, n in enumerate(actions)}
+            query_indices[actor.name] = {n: i for i, n in enumerate(queries)}
+
+            action_array = BlockPtrArray[String]([pool[s] for s in actions]) if actions else None
+            query_array = BlockPtrArray[String]([pool[s] for s in queries]) if queries else None
+            # todo: secondary name
+            actors.append(_Actor(actor.name, '', action_array, query_array, pool))
+            if action_array:
+                actor_fnames.append(action_array)
+            if query_array:
+                actor_fnames.append(query_array)
+
+        event_indices = {n: i for i, n in enumerate(events_)}
+        entrypoint_calls: Dict[str, List[int]] = {n.name: [] for n in entrypoints_}
+        for event in events_:
+            ev: _Event
+            evdata: Optional[_EventData]
+            if isinstance(event, ActionNode):
+                nxt = event_indices[event.out_edges[0]] if event.out_edges else 0xFFFF
+                actor_index = actor_indices[event.action.actor_name]
+                action_index = action_indices[event.action.actor_name][event.action.name]
+                params = Container(event.params, pool) if event.params else None
+
+                ev = _ActionEvent(event.name, nxt, actor_index, action_index, params, pool)
+                evdata = params
+            else:
+                raise TypeError(f'{type(event).__name__} not supported')
+
+            events.append(ev)
+            if evdata:
+                event_data.append(evdata)
+
+        for entrypoint in entrypoints_:
+            si = _SubflowIndexArray(entrypoint_calls[entrypoint.name]) or None
+
+            vardef_names: Optional[Dictionary] = None
+            epdata: _EntrypointData = _Pad24()
+            if entrypoint.vardefs:
+                vardef_names = Dictionary([v.name for v in entrypoint.vardefs], pool)
+                epdata = BlockArray[_VarDef]([
+                    _VarDef(TypedValue(v.type_, v.initial_value))
+                    for v in entrypoint.vardefs
+                ])
+            start = event_indices[entrypoint.out_edges[0]]
+            ep = _Entrypoint(vardef_names, epdata, si, start)
+
+            entrypoint_names.append(entrypoint.name)
+            entrypoints.append(ep)
+            entrypoint_data.append(epdata)
+
+        actor_array = BlockArray[_Actor](actors)
+        event_array = BlockArray[_Event](events)
+        entrypoint_dictionary = Dictionary(entrypoint_names, pool)
+        entrypoint_array = BlockArray[_Entrypoint](entrypoints)
+        event_data_array = BlockArray[_EventData](event_data)
+        entrypoint_data_array = BlockArray[_EntrypointData](entrypoint_data)
+
+        header = _FlowchartHeader(name, num_actions, num_queries, actor_array, event_array,
+                entrypoint_dictionary, entrypoint_array, pool)
+
+        super().__init__([
+            header,
+            actor_array,
+            event_array,
+            entrypoint_dictionary,
+            entrypoint_array,
+            event_data_array,
+            *actor_fnames,
+            entrypoint_data_array,
+        ])
+
+    def alignment(self) -> int:
+        return 8
---
+++ bfevfl/test_flowchart.py
@@ -0,0 +1,208 @@
+from __future__ import annotations
+
+import unittest
+
+from bitstring import Bits, pack
+
+from .datatype import TypedValue, IntType, FloatType, BoolType
+from .actors import Actor, Action, Query
+from .str_ import String, StringPool
+from .dic_ import Dictionary
+from .array import BlockArray, BlockPtrArray
+from .container import Container
+from .flowchart import (_Actor, _Event, _ActionEvent,
+        _SubflowIndexArray, _VarDef, _Pad24, _Entrypoint, _FlowchartHeader, Flowchart)
+
+class Test_Actor(unittest.TestCase):
+    def setUp(self):
+        self.sp = StringPool(['EventFlowSystemActor', 'sec_name'] +
+            [f'action{i}' for i in range(10)] +
+            [f'query{i}' for i in range(10)]
+        )
+
+    def test_empty(self):
+        actor = _Actor('EventFlowSystemActor', '', None, None, self.sp)
+
+        self.assertEqual(actor.get_all_pointers(), [0, 8, 16])
+        self.assertEqual(actor.prepare_bitstream(),
+            pack('uintle:64', self.sp['EventFlowSystemActor'].offset) +
+            pack('uintle:64', self.sp.empty.offset) +
+            pack('uintle:64', self.sp.empty.offset) +
+            Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0') +
+            Bits(b'\0\0\0\0\xff\xff\1')
+        )
+
+    def test_nonempty(self):
+        actions = BlockPtrArray[String]([self.sp[f'action{i}'] for i in range(10)])
+        queries = BlockPtrArray[String]([self.sp[f'query{i}'] for i in range(10)])
+        actions.offset = 218093421830921
+        queries.offset = 895719471249182
+        actor = _Actor('EventFlowSystemActor', 'sec_name', actions, queries, self.sp)
+
+        self.assertEqual(actor.get_all_pointers(), [0, 8, 16, 24, 32])
+        self.assertEqual(actor.prepare_bitstream(),
+            pack('uintle:64', self.sp['EventFlowSystemActor'].offset) +
+            pack('uintle:64', self.sp['sec_name'].offset) +
+            pack('uintle:64', self.sp.empty.offset) +
+            pack('uintle:64', actions.offset) +
+            pack('uintle:64', queries.offset) +
+            Bits(b'\0\0\0\0\0\0\0\0') +
+            Bits(b'\x0a\0\x0a\0\xff\xff\1')
+        )
+
+class Test_ActionEvent(unittest.TestCase):
+    def setUp(self):
+        self.sp = StringPool(['action'])
+
+    def test_no_params(self):
+        action = _ActionEvent('action', 0xFFFF, 0, 4, None, self.sp)
+
+        self.assertEqual(action.get_all_pointers(), [0])
+        self.assertEqual(action.prepare_bitstream(),
+                pack('uintle:64', self.sp['action'].offset) +
+                Bits(b'\0\0\xff\xff\0\0\4\0') +
+                pack('uintle:64', 0) +
+                Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
+        )
+
+    def test_params(self):
+        params = Container({}, self.sp)
+        params.offset = 21934812940
+        action = _ActionEvent('action', 1, 1, 3, params, self.sp)
+
+        self.assertEqual(action.get_all_pointers(), [0, 16])
+        self.assertEqual(action.prepare_bitstream(),
+                pack('uintle:64', self.sp['action'].offset) +
+                Bits(b'\0\0\1\0\1\0\3\0') +
+                pack('uintle:64', params.offset) +
+                Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')
+        )
+
+class Test_SubflowIndexArray(unittest.TestCase):
+    def test_empty(self):
+        arr = _SubflowIndexArray([])
+        self.assertEqual(arr.prepare_bitstream(), Bits(b''))
+
+    def test_nonempty(self):
+        arr = _SubflowIndexArray([0, 5, 4, 0x3321, 7])
+        self.assertEqual(arr.prepare_bitstream(), Bits(b'\0\0\5\0\4\0\x21\x33\7\0'))
+
+class Test_VarDef(unittest.TestCase):
+    def test_int(self):
+        v = _VarDef(TypedValue(IntType, 4))
+        self.assertEqual(v.prepare_bitstream(), Bits(b'\4\0\0\0\0\0\0\0\1\0\2\0'))
+
+    def test_bool_true(self):
+        v = _VarDef(TypedValue(BoolType, True))
+        self.assertEqual(v.prepare_bitstream(), Bits(b'\1\0\0\x80\0\0\0\0\1\0\3\0'))
+
+    def test_bool_false(self):
+        v = _VarDef(TypedValue(BoolType, False))
+        self.assertEqual(v.prepare_bitstream(), Bits(b'\0\0\0\0\0\0\0\0\1\0\3\0'))
+
+    def test_float(self):
+        v = _VarDef(TypedValue(FloatType, -500.32))
+        self.assertEqual(v.prepare_bitstream(), pack('floatle:32', -500.32) + Bits(b'\0\0\0\0\1\0\4\0'))
+
+class Test_Entrypoint(unittest.TestCase):
+    def test_simple(self):
+        ep = _Entrypoint(None, _Pad24(), None, 0x2345)
+
+        self.assertEqual(ep.get_all_pointers(), [])
+        self.assertEqual(ep.prepare_bitstream(),
+                Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x45\x23')
+        )
+
+    def test_subflow_index(self):
+        si = _SubflowIndexArray(list(range(10)))
+        si.offset = 1289471295
+        ep = _Entrypoint(None, _Pad24(), si, 0x2345)
+
+        self.assertEqual(ep.get_all_pointers(), [0])
+        self.assertEqual(ep.prepare_bitstream(),
+                pack('uintle:64', si.offset) +
+                Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x0a\0\0\0\x45\x23')
+        )
+
+    def test_vardef(self):
+        sp = StringPool(['hello', 'world'])
+        names = Dictionary(['hello', 'world'], sp)
+        vardefs = BlockArray[_VarDef]([
+            _VarDef(TypedValue(IntType, 5123)),
+            _VarDef(TypedValue(FloatType, 1e10)),
+        ])
+        vardefs.offset = 124513
+        ep = _Entrypoint(names, vardefs, None, 0x2345)
+
+        self.assertEqual(ep.get_all_pointers(), [8, 16])
+        self.assertEqual(ep.prepare_bitstream(),
+                Bits(b'\0\0\0\0\0\0\0\0') +
+                pack('uintle:64', names.offset) +
+                pack('uintle:64', vardefs.offset) +
+                Bits(b'\0\0\2\0\x45\x23')
+        )
+
+class Test_FlowchartHeader(unittest.TestCase):
+    def test(self):
+        pool = StringPool(['Flowchart'])
+        actors = BlockArray[_Actor]([])
+        actors.n = 5
+        events = BlockArray[_Event]([])
+        events.n = 1
+        entrypoint_names = Dictionary([], pool)
+        entrypoints = BlockArray[_Entrypoint]([])
+        entrypoints.n = 4
+
+        header = _FlowchartHeader('Flowchart', 3, 7, actors, events, entrypoint_names, entrypoints, pool)
+
+        header.offset = 0x80000
+        pool.offset = 0x100000
+        actors.offset = 0xabcdef01
+        events.offset = 0x01234567
+        entrypoint_names.offset = 0x456789ab
+        entrypoints.offset = 0x32324512
+
+        self.assertEqual(header.get_all_pointers(), [0x80020, 0x80028, 0x80030, 0x80038, 0x80040])
+        self.assertEqual(header.prepare_bitstream(),
+                Bits(b'EVFL\0\0\x08\0\0\0\0\0\0\0\0\0') +
+                Bits(b'\5\0\3\0\7\0\1\0\4\0\0\0\0\0\0\0') +
+                pack('uintle:64', pool['Flowchart'].offset + 2) +
+                pack('uintle:64', actors.offset) +
+                pack('uintle:64', events.offset) +
+                pack('uintle:64', entrypoint_names.offset) +
+                pack('uintle:64', entrypoints.offset)
+        )
+
+class TestFlowchart(unittest.TestCase):
+    def setUp(self):
+        self.sp = StringPool([
+            'Flowchart'
+        ] + [f'{s}{i}' for i in range(100) for s in ('Actor', 'Action', 'Query', 'Event')])
+        self.sp.offset = 0x1234
+
+    def test_empty(self):
+        fc = Flowchart('Flowchart', [], [], [], self.sp)
+
+        self.assertEqual(fc.prepare_bitstream(),
+            Bits(b'EVFL\x34\x12\0\0\0\0\0\0\0\0\0\0') +
+            Bits(b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0') +
+            pack('uintle:64', self.sp['Flowchart'].offset + 2) +
+            Bits(b'\x48\0\0\0\0\0\0\0\x48\0\0\0\0\0\0\0') +
+            Bits(b'\x48\0\0\0\0\0\0\0\x60\0\0\0\0\0\0\0') +
+            Bits(b'DIC \0\0\0\0\xff\xff\xff\xff\0\0\0\0') +
+            pack('uintle:64', self.sp.empty.offset)
+        )
+
+    def test_actor_filtering(self):
+        actors = [Actor(f'Actor{i}') for i in range(100)]
+        for actor in actors:
+            for i in range(50):
+                actor.register_action(Action(actor.name, f'Action{i}', []))
+                actor.register_query(Query(actor.name, f'Query{i}', [], IntType))
+        actors[0].actions['Action0'].mark_used()
+
+        fc = Flowchart('Flowchart', actors, [], [], self.sp)
+        self.assertLess(len(fc), 200)
+
+    # TODO more tests
+
